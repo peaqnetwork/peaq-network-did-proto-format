e15dc3cf52589cbb22988702f8effa3fc3bcfc41
Iredia Ebikade <iredia.developer@gmail.com>
2024-01-31 12:03:28 +0100

    feat: added rust did document  proto code gen

--- proto-code-gen.sh
+++ proto-code-gen.sh
@@ -2,9 +2,9 @@
 # protoc -I=./ --go_opt=paths=source_relative --go_out golang/document $(find ./ -iname "did_document_format.proto")
 
 # # Rust code generator
-# protoc -I=./  --rust_out rust/src $(find ./ -iname "*.proto")
-# # protoc -I=./  --rust_out rust/src $(find ./ -iname "did_document_format.proto")
-# #rename the mod.rs to lib.rs
+protoc -I=./  --rust_out rust/src $(find ./ -iname "*.proto")
+protoc -I=./  --rust_out rust/src $(find ./ -iname "did_document_format.proto")
+#rename the mod.rs to lib.rs
 # mv rust/src/mod.rs rust/src/lib.rs
 
 # # Dart code generator
